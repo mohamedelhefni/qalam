5e15c1c595d8b98e093507637592dfb6b84d0a29
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2023-04-24 13:15:22 +0200

    add vercel analytics

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,7 +1,8 @@
+import { inject } from "@vercel/analytics";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createPinia } from "pinia";
 let pinia = createPinia();
-
+inject();
 createApp(App).use(pinia).mount("#app");
