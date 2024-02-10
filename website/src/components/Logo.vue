c19dc5855e7d16b50eed314efd93d36346bf82df
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-02-10 16:28:06 +0200

    chore: make logo anchor link instead of deiv

--- website/src/components/Logo.vue
+++ website/src/components/Logo.vue
@@ -4,8 +4,8 @@ import { PhPenNib } from "@phosphor-icons/vue"
 </script>
 
 <template>
-    <div class="flex w-full gap-2 text-2xl">
-        <PhPenNib :size="30" class="text-base-800"  />
-        <p class="font-bold text-base-800">قلم</p>
-    </div>
+    <a href="/" class="flex w-full gap-2 text-2xl">
+            <PhPenNib :size="30" class="text-base-800" />
+            <p class="font-bold text-base-800">قلم</p>
+    </a>
 </template>
