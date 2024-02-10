85fce2e180fac3c47a08c34adb2e3cc3ea328d29
Mohamed Elhefni <elhefnymohamed446@gmail.com>
2024-02-10 16:25:05 +0200

    chore: make note content mobile friendly

--- website/src/components/NoteContent.vue
+++ website/src/components/NoteContent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { PhEye } from '@phosphor-icons/vue';
 import Showdown from 'showdown';
 import { beautifyDate } from '../utils/date';
 
@@ -8,16 +7,12 @@ const showDown = new Showdown.Converter()
 
 </script>
 <template>
-    <div class="container max-w-screen-lg mx-auto py-10" :dir="dir">
-        <div class="flex flex-col gap-2 ">
-            <p class="text-base-400 text-muted text-sm">{{ beautifyDate(note.created_at) }}</p>
-            <h1 class="text-5xl text-base-800 font-bold">{{ note.title }}</h1>
-            <div class="flex items-center gap-3 text-muted text-sm mt-1">
-                <PhEye :size="20" class="text-base-800" />
-                <p class="text-base-800">{{ note.views }}</p>
-            </div>
+    <div class="container max-w-screen-lg mx-auto py-10 px-10 lg:px-5" :dir="dir">
+        <div class="flex flex-col gap-2 items-center">
+            <p class="text-base-400 text-muted text-sm">{{ beautifyDate(note.created_at) }}, <span class="mx-1"><b>{{ note.views }}</b> مشاهده</span></p>
+            <h1 class="text-2xl lg:text-5xl text-base-800 font-bold">{{ note.title }}</h1>
         </div>
-        <div class=" my-10 prose-xl" v-html="showDown.makeHtml(note.content)">
+        <div class="my-10 prose lg:prose-xl mx-auto" v-html="showDown.makeHtml(note.content)">
         </div>
     </div>
 </template>
